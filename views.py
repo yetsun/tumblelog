0d0d9fd4fbce1d4892efec02a73785922f1e9630
Jun Ye <yetsun@gmail.com>
2014-09-24 01:05:41 -0700

    add comment

--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, redirect, render_template, url_for
 from flask.views import MethodView
 from tumblelog.models import Post, Comment
 
+from flask.ext.mongoengine.wtf import model_form
+
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
@@ -14,9 +16,40 @@ class ListView(MethodView):
 
 class DetailView(MethodView):
 
+    form = model_form(Comment, exclude=['created_at'])
+    
+    
+    def get_context(self, slug):
+        post = Post.objects.get_or_404(slug=slug)
+        form = self.form(request.form)
+
+        context = {
+            "post": post,
+            "form": form
+        }
+        return context
+
     def get(self, slug):
         post = Post.objects.get_or_404(slug=slug)
         return render_template('posts/detail.html', post=post)
+
+
+    
+    def post(self, slug):
+        context = self.get_context(slug)
+        form = context.get('form')
+
+        if form.validate():
+            comment = Comment()
+            form.populate_obj(comment)
+
+            post = context.get('post')
+            post.comments.append(comment)
+            post.save()
+
+            return redirect(url_for('posts.detail', slug=slug))
+
+        return render_template('posts/detail.html', **context)
 
 
 # Register the urls
